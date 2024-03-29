2ba6665ceeb4dced4d02d5a1eecd3f7a62f1056f
Kauan <raidoka546@gmail.com>
2024-03-28 21:18:53 -0300

    trying to show html content according to the nav item clicked

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,40 @@
+const placeToShowContent = document.getElementById('app');
 
 const anchors = document.querySelectorAll('a');
 anchors.forEach(anchor => {
     anchor.addEventListener('click', clickEventCatcher);
 });
 
-
 function clickEventCatcher(event) {
     event.preventDefault();
-
-    const url = event.target.href;
-    const currentUrl = window.location.href;
     
-    loadPageComponents(url)
+    const url = event.currentTarget.href;
+    const currentUrl = window.location.href;
+
+    //
+    const urlSegments = url.split('/');
+    const lastSegment = urlSegments[urlSegments.length - 1];
+
+    window.history.pushState({}, '', lastSegment);
+    
+    loadPageComponents(url, currentUrl);
 }
+
 
 function loadPageComponents(url) {
     fetch(url)
     .then(response => {
         if (!response.ok) {
-            throw new Error('Network response was not ok')
+            throw new Error('Nao deu bom');
         }
         return response.text();
     })
     .then(html => {
-        document.querySelector('#app').innerHTML = html;
+        placeToShowContent.innerHTML = html;
     })
     .catch(error => {
-        console.error('There was a problem with the fetch operation:', error)
+        console.error('Problema no fetch:', error)
     });
+
+    const bodyForBg = document.querySelector('body')
 }
---
+++ src/js/routes.js
@@ -0,0 +1,6 @@
+export const routes = {
+    '/' : '/index.html',
+    '/exploration' : '/pages/exploration.html',
+    '/home' : '/pages/home.html',
+    '/universe' : '/pages/universe.html'
+}
